68b318755a62a29abfde8529b8ea122a4519d95f
gusparck <araujorodrigues.guxtavo@gmail.com>
2025-01-09 15:39:26 -0300

    Protótipo inicial do menu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Sistema para Agendamento de Partidas de Tênis");
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Protótipo - Menu\n");
+        System.out.println("Selecione uma opção:\n1-Cadastrar usuário\n2-Fazer login\n3-Ranking de jogadores");
+        
+        int option = ler.nextInt();
+        switch(option){
+            case 1 -> System.out.println("Realizando cadastro.\n");
+            //adicionar método CADASTRO
+
+            case 2 -> System.out.println("Fazendo login.\n");
+            //adicionar método LOGIN
+            //uma fez feito o login o usuário pode buscar infos sobre outros usuários
+
+            case 3 -> System.out.println("Conferindo ranking dos melhores jogadores.\n");
+            //adicionar método RANKING
+        }
     }
 }
