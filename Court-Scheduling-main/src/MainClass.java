1851fcb35564b5e16ca9984db2347e1203adf3e2
gusparck <araujorodrigues.guxtavo@gmail.com>
2025-01-13 20:40:59 -0300

    99% fé e 1% sorte

--- Court-Scheduling-main/src/MainClass.java
+++ Court-Scheduling-main/src/MainClass.java
@@ -5,5 +5,6 @@ public class MainClass {
     public static void main(String[] args) {
         consoleMenu cm = new consoleMenu();
         cm.inicializador();
+        System.out.println("A magia da computação."); //só tá aqui pra poder dar commit na nova formatação de arquivos
     }
 }
