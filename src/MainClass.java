f384d56109c5360ab634b50a14ad5f3d7692e487
ce.dev777 <149310083+eliasDeveloo21@users.noreply.github.com>
2025-01-13 13:24:34 -0300

    Delete src directory

--- src/MainClass.java
+++
@@ -1,9 +0,0 @@
-import Console.ConsoleMenu;
-import Controle.Controle;
-//main principal do codigo, pra fins de organizacao mesmo, daqui o codigo inicia
-public class MainClass {
-    public static void main(String[] args) {
-        ConsoleMenu cm = new ConsoleMenu();
-        cm.inicializador();
-    }
-}
